e3dee8abf92f6dce8cfc476cccc9e84c078bb90b
Szymon <szymo@example.com>
2025-12-19 12:18:26 +0100

    fix(cors): Dodanie localhost:8000 do allowed_origins dla frontendu

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'http://localhost:8000',
+        'http://127.0.0.1:8000',
         'http://ls.test',
     ],
     'allowed_origins_patterns' => [],
